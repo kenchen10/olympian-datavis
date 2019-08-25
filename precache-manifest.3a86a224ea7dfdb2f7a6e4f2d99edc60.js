f82f4938fa951023ed193c9ba7333ee48a0b4e10
Kenny Chen <kenchen10@berkeley.edu>
2019-08-25 01:50:29 -0400

    Updates

---
+++ precache-manifest.3a86a224ea7dfdb2f7a6e4f2d99edc60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141bfac1bcf26491c9f4130950348b4b",
+    "url": "/olympian-datavis/index.html"
+  },
+  {
+    "revision": "c401f1d847fafcfc42cd",
+    "url": "/olympian-datavis/static/css/main.db02c544.chunk.css"
+  },
+  {
+    "revision": "99a5d9428190bf328e5a",
+    "url": "/olympian-datavis/static/js/2.7f298c37.chunk.js"
+  },
+  {
+    "revision": "c401f1d847fafcfc42cd",
+    "url": "/olympian-datavis/static/js/main.36b09460.chunk.js"
+  },
+  {
+    "revision": "91e3eb2250b1b2e19b0e",
+    "url": "/olympian-datavis/static/js/runtime~main.d851e030.js"
+  },
+  {
+    "revision": "72f24789b955954fa7cec618fe9dd101",
+    "url": "/olympian-datavis/static/media/bronze.72f24789.png"
+  },
+  {
+    "revision": "944f04907da1d9e6f1b00b12fcc6b355",
+    "url": "/olympian-datavis/static/media/gold.944f0490.png"
+  },
+  {
+    "revision": "94e277b35b521e8bfef0d840a52aceab",
+    "url": "/olympian-datavis/static/media/silver.94e277b3.png"
+  },
+  {
+    "revision": "23b9ef0417033ff3022915f73115d3ba",
+    "url": "/olympian-datavis/static/media/usa_rio_unique_names.23b9ef04.csv"
+  }
+]);
